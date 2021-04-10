2dae7d0fecf5f7ce0b1e647e30557335ef63cadf
1egend <>
2021-04-10 03:08:34 +0000

    Set up socket connection and basic multiplayer

--- main.js
+++ main.js
@@ -1,25 +1,85 @@
 var socket = io();
-socket.on('message', function(data) {
+socket.on('helloworld', function(data) {
   console.log(data);
 });
 
+/** GLOBALS  */
+// Keypressed
+var k = {
+  up: false,
+  down: false,
+  left: false,
+  right: false
+};
+
+// Canvas width and height
+var w = 800, h = 600;
+var fps = 60;
+
+/** END OF GLOBALS */
+
 var canvas = document.getElementById('canvas');
-canvas.width = 800;
-canvas.height = 600;
+canvas.width = w;
+canvas.height = h;
 
 /** Custom written wrapper for CanvasRenderingContext2D **/ 
-function circle(x, y, r){
-  context.beginPath();
-  context.arc(x, y, r, 0, 2 * Math.PI);
-  context.fill();
-}
-
 var ctx = canvas.getContext('2d');
+
+function circle(x, y, r){
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fill();
+}
+/** WRAPPER END */
+
 socket.on('state', function(players) {
-  ctx.clearRect(0, 0, 800, 600);
+  ctx.clearRect(0, 0, w, h);
   ctx.fillStyle = 'green';
   for (var id in players) {
     var player = players[id];
     circle(player.x, player.y, 10);
   }
 });
+
+// WASD to move
+document.addEventListener('keydown', function(event) {
+  switch (event.keyCode) {
+    case 65: // A
+      k.left = true;
+      break;
+    case 87: // W
+      k.up = true;
+      break;
+    case 68: // D
+      k.right = true;
+      break;
+    case 83: // S
+      k.down = true;
+      break;
+  }
+});
+
+document.addEventListener('keyup', function(event) {
+  switch (event.keyCode) {
+    case 65: // A
+      k.left = false;
+      break;
+    case 87: // W
+      k.up = false;
+      break;
+    case 68: // D
+      k.right = false;
+      break;
+    case 83: // S
+      k.down = false;
+      break;
+  }
+});
+
+// New player has joined
+socket.emit('new player');
+
+// Send key info at fps
+setInterval(function() {
+  socket.emit('keypressed', k);
+}, 1000 / fps);
--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const app = express();
 const server = http.Server(app);
 const io = socketIO(server);
 
-const port = 7777;
+const port = 3000;
 
 app.set('port', port);
 
+app.use('/', express.static(__dirname + '/'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -19,11 +20,41 @@ server.listen(port, () => {
   console.log(`Server started at port: ${port}`);
 });
 
+// Stores player info
+var players = {};
+
 // sockets
 io.on('connection', function(socket) {
-  
+  socket.on('new player', function() {
+    players[socket.id] = {
+      x: 400,
+      y: 300
+    };
+  });
+  socket.on('keypressed', function(k) {
+    var player = players[socket.id] || {};
+    if (k.left) {
+      player.x -= 3;
+    }
+    if (k.up) {
+      player.y -= 3;
+    }
+    if (k.right) {
+      player.x += 3;
+    }
+    if (k.down) {
+      player.y += 3;
+    }
+  });
 });
 
+// Send player states back at 60fps
 setInterval(function() {
-  io.sockets.emit('message', 'hi!');
-}, 1000);
+  io.sockets.emit('state', players);
+}, 1000 / 60);
+
+/**
+  setInterval(function() {
+    io.sockets.emit('helloworld', 'Send this every second!');
+  }, 1000);
+**/
