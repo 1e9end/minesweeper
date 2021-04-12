6acca3c7db8068828d14daa68d7423b2d52511c6
1egend <>
2021-04-12 22:25:49 +0000

    Finished singleplayer

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 var socket = io();
-socket.on('helloworld', function(data) {
-  console.log(data);
-});
 
 /** GLOBALS  */
 // Keypressed
@@ -32,54 +29,102 @@ function circle(x, y, r){
 }
 /** WRAPPER END */
 
-socket.on('state', function(players) {
-  ctx.clearRect(0, 0, w, h);
-  ctx.fillStyle = 'green';
-  for (var id in players) {
-    var player = players[id];
-    circle(player.x, player.y, 10);
+// Mouse
+var mouse = {
+  x: 0,
+  y: 0,
+  clicked: false,
+  button: false
+}
+
+socket.on('state', function(games) {
+  var map = games[socket.id];
+  function setup(){
+    createCanvas(w, h);
   }
+
+  function draw(){
+      background(0);
+      textAlign(CENTER, CENTER);
+      textSize(20);
+      var m = map.m;
+      var c = map.c;
+      
+      var ww = w/map.s;
+      var hh = h/map.s;
+
+      for (var y = 0; y < m.length; ++y){
+          for (var x = 0; x < m[y].length; ++x){
+              fill(0, 0, 0, 80);
+              if (c[y][x] >= 0){
+                  fill(0, 0, 0, 50);
+              }
+              rect(x * ww, y * hh, ww, hh);
+              if (c[y][x] > 0){
+                  fill(0);
+                  text(c[y][x], x * ww +  1/2 * ww, 1/2 * hh + y * hh); 
+              }
+              if (c[y][x] === -2){
+                  fill(0);
+                  rect(x * ww + ww/5, y * hh + hh/5, ww /10, hh * 3/5);
+                  fill(255, 0, 0);
+                  triangle(x * ww + ww/5, y * hh + hh/5, x * ww + ww/5, y * hh + hh * 3/5, x * ww + ww * 4/5, y * hh + hh * 2/5);
+              }
+              if (c[y][x] === -3){
+                  fill(0);
+                  ellipse(x * ww +  1/2 * hh, 1/2 * hh + y * hh, ww * 4/5, hh * 4/5);
+              }
+          }
+      }  
+      
+      var scene = map.scene;
+      if (scene === 0){
+          textSize(100);
+          fill(0);
+          text("Minesweeper", w/2, h/3);
+          textSize(40);
+          text("Click somewhere (hopefully\nnot a bomb) to start", w/2, h * 2/3);
+      }
+      if (scene === 2){
+          textSize(100);
+          fill(0);
+          if (lose){
+              text("YoU lOsT", w/2, h/3);
+              textSize(40);
+              text("Better luck next time!\n Restart to replay", w/2, h * 2/3);
+          }
+          else{
+              text("YoU wOn", w/2, h/3);
+          }
+      }
+      
+      mouse.clicked = false;
+  };
+
+  function mouseClicked(){
+      mouse.clicked = true;
+      mouse.button = mouseButton;
+      mouse.x = mouseX;
+      mouse.y = mouseY;
+  };
 });
 
-// WASD to move
-document.addEventListener('keydown', function(event) {
-  switch (event.keyCode) {
-    case 65: // A
-      k.left = true;
-      break;
-    case 87: // W
-      k.up = true;
-      break;
-    case 68: // D
-      k.right = true;
-      break;
-    case 83: // S
-      k.down = true;
-      break;
-  }
-});
+// Best browser performance requestAnimationFrame
+window.reqAnimationFrame = (function(callback){
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+    function(callback) {
+      window.setTimeout(callback, 1000/fps);
+    };
+})();
 
-document.addEventListener('keyup', function(event) {
-  switch (event.keyCode) {
-    case 65: // A
-      k.left = false;
-      break;
-    case 87: // W
-      k.up = false;
-      break;
-    case 68: // D
-      k.right = false;
-      break;
-    case 83: // S
-      k.down = false;
-      break;
-  }
-});
+function sendInfo(){
+    socket.emit('mouse', mouse);
+    
+    window.reqAnimationFrame(sendInfo, 1000/fps);
+}
 
-// New player has joined
-socket.emit('new player');
+window.addEventListener("load", function(){
+  socket.emit('new player');
+}, false);
 
-// Send key info at fps
-setInterval(function() {
-  socket.emit('keypressed', k);
-}, 1000 / fps);
+window.addEventListener("load", sendInfo, false);
